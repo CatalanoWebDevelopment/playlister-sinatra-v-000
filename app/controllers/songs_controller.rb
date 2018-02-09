0e484b7eaca9f06b738dedf7a6b9c9535ed60f72
Parker Catalano <catalano.work@gmail.com>
2018-02-09 16:32:13 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,7 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully updated song."
-    redirect '/songs/#{@song.slug}'
+    redirect "/songs/#{@song.slug}""
   end
 
 end
