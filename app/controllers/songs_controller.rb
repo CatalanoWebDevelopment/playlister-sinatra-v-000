8cd20c1ab0d3a92cbef20ef342d7a6a3275ecc44
Parker Catalano <catalano.work@gmail.com>
2018-02-09 16:06:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,7 +39,7 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Song.find_or_create_by
+    @song.artist = Song.find_or_create_by(params["song"]["artist"])
 
   end
 
